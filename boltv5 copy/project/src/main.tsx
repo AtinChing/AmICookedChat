b1b20b27904e6b3341a52e4bcc54cb581aa48f10
Mudit Arora <44677855+Mudit-Arora@users.noreply.github.com>
2025-04-20 10:49:51 -0700

    remove key

--- boltv5 copy/project/src/main.tsx
+++ boltv5 copy/project/src/main.tsx
@@ -7,8 +7,8 @@ import { Auth0Provider } from '@auth0/auth0-react';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Auth0Provider
-      domain="genai-05808952325237127.us.auth0.com"
-      clientId="3HpRI94SGTfB2SPAp7rZvgzn1BpOtkuI"
+      domain="DOMAIN_KEY"
+      clientId="CLIENT_ID"
       authorizationParams={{
         redirect_uri: window.location.origin + '/callback'
       }}
